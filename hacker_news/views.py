a88fb0b7f026c56404db0b3742fd81c1c30ddec6
Andrey <ayanov@deloitte.com>
2019-12-25 19:55:23 +0200

    Optimize retrieval of latest votes value for stories

--- hacker_news/views.py
+++ hacker_news/views.py
@@ -1,8 +1,7 @@
 from django.views.generic import TemplateView
 
-from hacker_news.tasks import get_new_stories, update_story_votes
 from hacker_news.models import Story, StoryVotes
-from django.db.models import Max
+from django.db.models import OuterRef, Subquery
 
 
 MIN_SCORE = 100
@@ -13,13 +12,28 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         ctx = super(IndexView, self).get_context_data(**kwargs)
-        # get_new_stories.apply()
-        # update_story_votes.apply()
+        sort_by = self.request.GET.get('sort_by')
+        sorting_functions = {
+            'votes': lambda s: -s.latest_votes(),
+            'latest': lambda s: -s.posted.timestamp(),
+        }
+        if sort_by not in sorting_functions:
+            sort_by = 'latest'
+
+        latest_vote = StoryVotes \
+            .objects \
+            .filter(story=OuterRef('pk')) \
+            .filter(votes__gt=MIN_SCORE) \
+            .filter(votes__isnull=False) \
+            .order_by('-tstamp')
+        stories = Story \
+            .objects \
+            .annotate(latest_vote=Subquery(latest_vote.values('votes')[:1])) \
+            .filter(latest_vote__isnull=False)
         ctx.update({
             'stories':
-                sorted([s for s in Story.objects.all()
-                        if s.latest_votes() and s.latest_votes() > MIN_SCORE],
-                       key=lambda s: -s.latest_votes()),
+                sorted(stories, key=sorting_functions[sort_by]),
         })
 
         return ctx
+
